30d3989c3be65d5f063cfa8f4859ac27526ec8a2
Robert Prine <robertprine2@yahoo.com>
2016-09-09 20:49:46 -0400

    bah

--- auth.js
+++ auth.js
@@ -3,7 +3,8 @@ module.exports = {
 	'googleAuth' : {
 		'clientID' 		: '794016409691-23utaecc07q7fi18ef8uuhmfava3sjlr.apps.googleusercontent.com',
 		'clientSecret'	: 'ANzbUL9s1iRkaChfCTr4hvrk',
-		'callbackURL' 	: 'http://localhost:3000/login/google/return', //http://localhost:8080/auth/google/callback
+		'callbackURL' 	: 'http://intense-reaches-75398.herokuapp.com/login/google/return',
+		//'callbackURL' 	: 'http://localhost:3000/login/google/return', //http://localhost:8080/auth/google/callback
 	},
 
 	googleMapAPIKey: 'AIzaSyBoY3tyaLpbkWArJ-UIWM2h33Q_KNO0N-U',
